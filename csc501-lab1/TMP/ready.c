79fe243720c522c6f600d14d87bb2698cdf11fd9
Houjun Tang <houj.tang@gmail.com>
2014-02-04 23:23:54 -0500

    ready.c

--- csc501-lab1/TMP/ready.c
+++ csc501-lab1/TMP/ready.c
@@ -4,6 +4,7 @@
 #include <kernel.h>
 #include <proc.h>
 #include <q.h>
+#include <lab1.h>
 
 /*------------------------------------------------------------------------
  * ready  --  make a process eligible for CPU service
@@ -11,6 +12,7 @@
  */
 int ready(int pid, int resch)
 {
+	//kprintf("%d:Entered ready()\n",pid);
 	register struct	pentry	*pptr;
 
 	if (isbadpid(pid))
@@ -18,7 +20,23 @@ int ready(int pid, int resch)
 	pptr = &proctab[pid];
 	pptr->pstate = PRREADY;
 	insert(pid,rdyhead,pptr->pprio);
+	
+
+	// insert pid to corresponding queue
+	int tmpprio = pptr->pprio;
+	if(tmpprio >=66 && tmpprio <=99) {
+		ht_dequeue(1);
+	}
+	else if(tmpprio >=33 && tmpprio <=65) {
+		ht_dequeue(2);
+	}
+	else {
+		ht_dequeue(3);
+	}
+
 	if (resch)
 		resched();
+	
+	//kprintf("%d:Exited ready()\n",pid);
 	return(OK);
 }
